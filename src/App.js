9283523d266016fa3c299f3e50b436d1ca690267
Dixit3112 <139951435+Dixit3112@users.noreply.github.com>
2024-02-29 17:12:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,38 +9,6 @@ function App() {
     <>
       <div>
         <Main />
-        {/* <div className=" font-extrabold text-lg text-[#852828] w-60 h-60 rounded-xl bg-sky-600 m-5 place-items-center text-center flex justify-center items-center uppercase ">
-          My Self Dixit Rakholiya.
-        </div>
-        <div class="chat-notification p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          <div class="chat-notification-logo-wrapper shrink-0">
-            <img class="chat-notification-logo h-12 w-12" alt="ChitChat Logo" />
-          </div>
-          <div class="chat-notification-content">
-            <h4 class="chat-notification-title text-xl font-medium text-black">
-              ChitChat
-            </h4>
-            <p class="chat-notification-message text-slate-500">
-              You have a new message!
-            </p>
-          </div>
-        </div>
-        <div class="py-8 px-8 my-11 max-w-sm mx-auto bg-blue rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-          <img
-            class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
-            src="/img/erin-lindford.jpg"
-            alt="Woman's Face"
-          />
-          <div class="text-center space-y-2 sm:text-left">
-            <div class="space-y-0.5">
-              <p class="text-lg text-black font-semibold">Erin Lindford</p>
-              <p class="text-slate-500 font-medium">Product Engineer</p>
-            </div>
-            <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-              Message
-            </button>
-          </div>
-        </div> */}
       </div>
     </>
   );
